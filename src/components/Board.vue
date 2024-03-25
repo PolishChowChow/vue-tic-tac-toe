157710e794f78f63533f85e86292ea386c987bdf
polishchowchow <bmaselko98@gmail.com>
2024-03-25 09:30:55 +0100

    initial files

---
+++ src/components/Board.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import Square from "./Square.vue";
+import { ref } from "vue"
+let board = ref<string[]>(["", "", "", "", "", "", "", "", ""]);
+let currentSign = "O";
+let isWinner = false;
+const setBoard = (internalIndex: number) => {
+    console.log(internalIndex);
+    if(board.value[internalIndex]!=='' || isWinner){
+        return;
+    }
+    board.value[internalIndex] = currentSign;
+    currentSign = currentSign === "O" ? "X" : "O";
+    checkWinner()
+    console.log(board);
+    
+}
+function checkWinner(){
+    return undefined;
+}
+</script>
+
+<template>
+  <div class="board">
+    <div class="square" v-for="(square, index) in board" :key="index">
+      <Square :content="square" :internalIndex="index" :handler="setBoard"> </Square>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.board {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
+</style>
---
+++ src/components/Square.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const { content, internalIndex, handler } = defineProps<{
+  content: string;
+  internalIndex: number;
+  handler: (internalIndex: number) => void;
+}>();
+const onClickHandler = (_: Event) => {
+  handler(internalIndex);
+};
+</script>
+
+<template>
+  <div class="square" @click="onClickHandler">
+    {{ content }}
+  </div>
+</template>
+
+<style scoped>
+.square {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  width: 100px;
+  height: 100px;
+  font-size: 20px;
+  border: 1px solid white;
+}
+</style>
